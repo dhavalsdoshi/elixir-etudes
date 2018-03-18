c956316261b6eacf52adb886fc86869d9e26c98d
Dhaval <dhaval.doshi@gmail.com>
2018-03-18 18:19:53 +0100

    adding default arguments and sample input outputs for area

--- chp2/geom.ex
+++ chp2/geom.ex
@@ -1,5 +1,14 @@
 defmodule Geom do
-  def area(length, width) do
+  def area(length \\ 1, width \\ 1) do
     length * width
   end
 end
+
+#iex(2)> Geom.area(1)
+#1
+#iex(3)> Geom.area(7)
+#7
+#iex(4)> Geom.area(1,4)
+#4
+#iex(5)> Geom.area(4,6)
+#24
